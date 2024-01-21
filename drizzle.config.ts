b146f51990407f65b57e55621273da6206c28e65
Ben Kunkle <ben.kunkle@gmail.com>
2024-01-20 20:32:09 -0800

    comment out drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { type Config } from "drizzle-kit";
 
 import { env } from "@/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "mysql2",
-  dbCredentials: {
-    uri: env.DATABASE_URL,
-  },
-  tablesFilter: ["seating_*"],
-} satisfies Config;
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   driver: "mysql2",
+//   dbCredentials: {
+//     uri: env.DATABASE_URL,
+//   },
+//   tablesFilter: ["seating_*"],
+// } satisfies Config;
