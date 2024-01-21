e560060358e3c5a9713fbea4b036ab276cff3013
Ben Kunkle <ben.kunkle@gmail.com>
2024-01-21 13:43:26 -0800

    rework dnd to use @dnd-kit

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-import { useState, type DragEventHandler, useRef, useMemo, type DragEvent } from "react";
+import {
+    useState,
+    useRef,
+    useMemo,
+    useEffect,
+} from "react";
 import { produce } from "immer";
+import { Dnd } from "./dnd";
 
 type Seat = {
     id: number;
@@ -9,7 +15,7 @@ type Seat = {
     y: number;
 };
 
-export function Canvas() {
+function useSeats() {
     const [seats, setSeats] = useState<Seat[]>([]);
     const [id, setId] = useState(0);
 
@@ -36,77 +42,141 @@ export function Canvas() {
         );
     }
 
+    function addDelta(id: number, x: number, y: number) {
+        setSeats(
+            produce((seats) => {
+                const i = seats.findIndex((s) => s.id === id);
+                if (i === -1) {
+                    return seats;
+                }
+                seats[i]!.x += x;
+                seats[i]!.y += y;
+            }),
+        );
+    }
+
+    return {
+        seats,
+        addSeat,
+        setSeatOffset,
+        addDelta,
+        nextId: id,
+    };
+}
+
+type Point = { x: number; y: number };
+
+function useCentroids() {
+    type Centroid = Point & { id: number };
+    const [centroids, setCentroids] = useState<Centroid[]>([]);
+
+    function update(centroid: Centroid) {
+        const i = centroids.findIndex((c) => c.id === centroid.id);
+        if (i === -1) {
+            setCentroids((cs) => [...cs, centroid]);
+            return;
+        }
+        setCentroids(
+            produce((cs) => {
+                cs[i]!.x = centroid.x;
+                cs[i]!.y = centroid.y;
+            }),
+        );
+    }
+    useEffect(() => {
+        console.log("centroids", centroids);
+    }, [centroids]);
+
+    return {
+        data: centroids,
+        update,
+    };
+}
+
+export function Canvas() {
     const dropRef = useRef<HTMLDivElement | null>(null);
 
-    const onDrop: DragEventHandler<HTMLDivElement> = (e) => {
-        const id = parseInt(e.dataTransfer.getData("id"));
-        const offset = getDragOffset(e)
-        // TODO: handle err (ofsx | ofsy = NaN)
+    const { seats, addSeat, addDelta } = useSeats();
+    const centroids = useCentroids();
 
-        const drop = dropRef.current;
-        if (!drop) return;
-        const [x, y] = calculateOffset(drop, e, offset);
-
-        console.log("dropped", id, {
-            abs: {
-                x,
-                y,
-            },
-            offset,
-        });
-        if (isNaN(id)) {
-            addSeat(x, y);
-            return
-        }
-        setSeatOffset(id, x, y)
-    };
     return (
-        <>
-            <div
+        <Dnd.Context onDragEnd={handleDragEnd}>
+            <Dnd.Droppable
+                id={CANVAS_DROPPABLE_ID}
                 className="relative min-h-[600px] min-w-[80%] bg-white"
-                ref={dropRef}
-                onDragOver={(e) => e.preventDefault()}
-                onDrop={onDrop}
             >
-                {seats.map((s) => (
-                    <Seat key={s.id} seat={s} />
-                ))}
-            </div>
+                <div ref={dropRef}>
+                    {seats.map((s) => (
+                        <DraggableSeat key={s.id} seat={s} />
+                    ))}
+                    {centroids.data.map((c) => (
+                        <div
+                            key={c.id}
+                            className="absolute z-10 h-4 w-4 rounded-full bg-red-500"
+                            style={{
+                                left: c.x,
+                                top: c.y,
+                                transform: "translate(-50%, -50%)",
+                            }}
+                        ></div>
+                    ))}
+                </div>
+            </Dnd.Droppable>
             <div className="h-[600px] min-w-28 border-l-2 border-l-black bg-white pl-4">
-                <Seat />
+                <DraggableSeat />
             </div>
-        </>
+        </Dnd.Context>
+    );
+
+    function handleDragEnd(e: Dnd.DragEndEvent) {
+        console.log("drag end", e);
+        const id = e.active.id as "new" | `${number}`;
+        if (id == "new") {
+            const cbb = dropRef.current?.getBoundingClientRect()
+            const [x,y] = calcCoords(e, cbb)
+            console.log({ id, x, y });
+            addSeat(x, y);
+            return;
+        }
+        const { x, y } = e.delta;
+        addDelta(parseInt(id), x, y);
+    }
+}
+
+function calcCoords(e: Dnd.DragEndEvent, dzOfs?: {left: number, top: number}) {
+    const activator = e.activatorEvent as MouseEvent;
+    const origX = activator.clientX;
+    const origY = activator.clientY;
+    const ofsX = activator.offsetX;
+    const ofsY = activator.offsetY;
+    const dzOfsX = dzOfs?.left ?? 0
+    const dzOfsY = dzOfs?.top ?? 0
+    const x = origX - ofsX - dzOfsX;
+    const y = origY - ofsY - dzOfsY;
+    return [x,y] as const
+
+}
+
+const CANVAS_DROPPABLE_ID = "seating-chart";
+
+function DraggableSeat(props: { seat?: Seat }) {
+    const seat = props.seat ?? { id: "new" };
+    return (
+        <Dnd.Draggable id={seat.id + ""} data={seat}>
+            <Seat seat={props.seat} />
+        </Dnd.Draggable>
     );
 }
 
-function Seat(props: { seat?: Seat}) {
-    const id = props.seat && props.seat.id + "";
-    const seatRef = useRef<HTMLDivElement | null>(null);
+function Seat(props: { seat?: Seat }) {
+    const id = props.seat?.id ?? -1;
 
-    const onDragStart: DragEventHandler<HTMLDivElement> = (e) => {
-        const seat = seatRef.current;
-        if (!seat) return;
-        setDragOffset(seat, e)
-        if (id) {
-            e.dataTransfer.setData("id", id);
-        }
-    };
-    const style = useMemo(() => {
-        if (!props.seat) {
-            return;
-        }
-        return {
-            top: props.seat.y,
-            left: props.seat.x,
-        };
-    }, [props.seat]);
+    const style = useSeatPos(props.seat);
+
     return (
         <div
-            id={id}
-            ref={seatRef}
-            draggable
-            onDragStart={onDragStart}
-            className="align-center absolute h-24 w-24 border-2 border-black text-center text-black"
+            id={id ? id + "" : undefined}
+            className="align-center absolute h-24 w-24 border-2 border-black bg-white text-center text-black"
             style={style}
         >
             {props.seat?.id}
@@ -114,37 +184,16 @@ function Seat(props: { seat?: Seat}) {
     );
 }
 
-function calculateOffset(
-    dropzone: HTMLElement,
-    dropped: { clientX: number; clientY: number },
-    offset_?: { x: number; y: number },
-) {
-    const offset = Object.assign({ x: 0, y: 0 }, offset_);
+function useSeatPos(seat?: Seat) {
+    const style = useMemo(() => {
+        if (!seat) {
+            return;
+        }
+        return {
+            top: seat.y,
+            left: seat.x,
+        };
+    }, [seat]);
 
-    const rect = dropzone.getBoundingClientRect();
-    const { left: dzX, top: dzY } = rect;
-
-    const { clientX: dX, clientY: dY } = dropped;
-
-    const x = dX - dzX - offset.x;
-    const y = dY - dzY - offset.y;
-
-    return [x, y] as const;
+    return style;
 }
-
-const OFFSET_X = "x"
-const OFFSET_Y = "y"
-
-function setDragOffset<Elem extends HTMLElement>(elem: Elem, e: DragEvent<Elem>) {
-    const [x, y] = calculateOffset(elem, e);
-    e.dataTransfer.setData(OFFSET_X, x + "");
-    e.dataTransfer.setData(OFFSET_Y, y + "");
-}
-
-function getDragOffset<Elem extends HTMLElement>(e: DragEvent<Elem>) {
-    const x = parseInt(e.dataTransfer.getData(OFFSET_X))
-    const y = parseInt(e.dataTransfer.getData(OFFSET_Y))
-    // TODO: handle x, y = NaN
-    return {x, y}
-}
-
