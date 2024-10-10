7407fe1f54f9826581307812a793bdf7c95cf10d
Ben Kunkle <ben.kunkle@gmail.com>
2024-10-10 15:48:08 -0500

    convert canvas to grid and always use snap coords

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -11,6 +11,22 @@ enableMapSet();
 
 const NEW_ID = "new" as const;
 
+const SNAP_THRESHOLD = 120;
+
+// NOTE: this is the offset controlled by browser or @dnd-kit (idk which) giving the
+// dragged element a lifted effect
+const DRAG_EXISTING_Y_OFFSET = 10;
+const SNAP_PAD = 5;
+const SEATING_CHART_DROPPABLE_ID = "seating-chart";
+
+const SEAT_GRID_W = 3
+const SEAT_GRID_H = 3
+
+const GRID_W = 120
+const GRID_H = 80
+
+const GRID_CELL_PX = 24
+
 type id = number;
 
 type newId = id | typeof NEW_ID;
@@ -144,6 +160,222 @@ const seatStore = create<SeatStore>()(
     })),
 );
 
+
+
+export function Canvas() {
+    const dropRef = useRef<HTMLDivElement | null>(null);
+
+    const addSeat = seatStore((s) => s.addSeat);
+    const addDelta = seatStore((s) => s.addDelta);
+    const offsets = seatStore((s) => s.offsets);
+    const setSeatOffset = seatStore((s) => s.setSeatOffset);
+
+    const setActive = useSetActive();
+    const setPreview = seatStore((s) => s.setPreview);
+
+    const stopDrag = seatStore((s) => s.stopDrag);
+
+    return (
+        <Dnd.Context
+            onDragEnd={handleDragEnd}
+            onDragStart={handleDragStart}
+            onDragMove={handleDragMove}
+        >
+            <Dnd.Droppable
+                id={SEATING_CHART_DROPPABLE_ID}
+                className="relative bg-white"
+                style={{
+                    height: GRID_H * GRID_CELL_PX,
+                    width: GRID_W * GRID_CELL_PX,
+                    backgroundImage: `
+                        linear-gradient(to right, #e5e5e5 1px, transparent 1px),
+                        linear-gradient(to bottom, #e5e5e5 1px, transparent 1px)
+                    `,
+                    backgroundSize: `${GRID_CELL_PX}px ${GRID_CELL_PX}px`,
+                }}
+            >
+                <div ref={dropRef}>
+                    <DropPreview />
+                    <Seats />
+                </div>
+                <Centroids />
+            </Dnd.Droppable>
+            <div className="h-[600px] min-w-28 border-l-2 border-l-black bg-white pl-4">
+                <DraggableSeat />
+            </div>
+        </Dnd.Context>
+    );
+
+    function handleDragStart(e: Dnd.DragStartEvent) {
+        const offset = offsets.get(e.active.id as id);
+        if (!offset) {
+            return;
+        }
+        console.log("drag start", offset);
+        const active = Object.assign(offset, { id: e.active.id as newId });
+        setActive(active);
+    }
+
+    function handleDragMove(e: Dnd.DragMoveEvent) {
+        // console.log("drag over", e)
+        const id = e.active.id as newId;
+
+        if (e.over === null && id === "new") {
+            setActive(null);
+            return;
+        }
+        const snapCoords = getSnapCoords(dropRef, e);
+        if (snapCoords === null) {
+            return
+        }
+        setActive(Object.assign(snapCoords, { id }));
+        setPreview(snapCoords);
+    }
+
+    function handleDragEnd(e: Dnd.DragEndEvent) {
+        console.log("drag end", e);
+        const id = parseId(e.active.id);
+        const snapCoords = getSnapCoords(dropRef, e);
+        if (snapCoords == null) {
+            return;
+        }
+        if (id == "new") {
+            const coords = snapCoords;
+            addSeat(coords.x, coords.y);
+            stopDrag();
+            return;
+        }
+        if (snapCoords) {
+            setSeatOffset(id, snapCoords);
+            return;
+        }
+        addDelta(id, e.delta);
+        stopDrag();
+    }
+}
+
+function parseId(id: number | string): newId {
+    if (typeof id === "number" || id === "new") {
+        return id;
+    }
+    const idInt = Number.parseInt(id);
+    if (!Number.isSafeInteger(idInt)) {
+        console.error(`invalid id ${id}`);
+        return "new";
+    }
+    return idInt;
+}
+
+function getSnapCoords(
+    dzRef: RefObject<HTMLDivElement>,
+    dragEvent: Dnd.DragEvent
+) {
+    const dzDims = dzRef.current?.getBoundingClientRect();
+    if (!dzDims) {
+        console.log("no dz dims");
+        return null;
+    }
+
+    const { clientX, clientY } = dragEvent.activatorEvent as MouseEvent;
+    const x = clientX - dzDims.left + dragEvent.delta.x;
+    const y = clientY - dzDims.top + dragEvent.delta.y;
+
+    const gridX = Math.floor(x / GRID_CELL_PX);
+    const gridY = Math.floor(y / GRID_CELL_PX);
+
+    return {
+        x: gridX * GRID_CELL_PX,
+        y: gridY * GRID_CELL_PX
+    };
+}
+
+type Side = "above" | "below" | "left" | "right";
+
+
+function distance(a: Point, b: Point) {
+    return Math.floor(
+        Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)),
+    );
+}
+
+
+function DropPreview() {
+    const active = seatStore((s) => s.preview);
+    return (
+        active && (
+            <div
+                className="absolute h-24 w-24 bg-blue-200"
+                style={{ left: active.x, top: active.y, height: SEAT_GRID_H * GRID_CELL_PX, width: SEAT_GRID_W * GRID_CELL_PX }}
+            ></div>
+        )
+    );
+}
+
+function DraggableSeat(props: { seatId?: id }) {
+    const id: newId = props.seatId ?? "new";
+    const offset = useSeatOffset(id);
+    return (
+        <Dnd.Draggable id={id + ""} data={{ id }}>
+            <Seat id={id} offset={offset} />
+        </Dnd.Draggable>
+    );
+}
+
+function Seats() {
+    const seats = useSeats();
+    return seats.map((s) => <DraggableSeat key={s} seatId={s} />);
+}
+
+function Seat(props: { id: newId; offset?: { x: number; y: number } }) {
+    const style = Object.assign({}, useSeatPos(props.offset), {
+        height: SEAT_GRID_H * GRID_CELL_PX,
+        width: SEAT_GRID_W * GRID_CELL_PX,
+    });
+    const setSeatRef = useSetSeatRef(props.id);
+    return (
+        <div
+            ref={setSeatRef}
+            id={props.id + ""}
+            className="align-center border-2 border-black bg-white text-center text-black"
+            style={style}
+        >
+            {props.id}
+        </div>
+    );
+}
+
+function Centroids() {
+    const centroids = useCentroids();
+    return centroids.map(([id, c]) => (
+        <div
+            key={id}
+            className="absolute z-10 h-4 w-4 rounded-full bg-red-500"
+            style={{
+                left: c.x,
+                top: c.y,
+                transform: "translate(-50%, -50%)",
+            }}
+        ></div>
+    ));
+}
+
+function useSeatPos(seat?: { x: number; y: number }) {
+    const style = useMemo(() => {
+        if (!seat) {
+            return {
+                position: "unset" as const,
+            };
+        }
+        return {
+            position: "absolute" as const,
+            top: seat.y,
+            left: seat.x,
+        };
+    }, [seat]);
+
+    return style;
+}
+
 function updateCentroid(state: Draft<SeatStore>, id: newId) {
     const ref = state.refs.get(id);
     const offset = id !== "new" ? state.offsets.get(id) : undefined;
@@ -276,312 +508,4 @@ function getClosestCentroid(id: newId) {
         distance: closestDistance,
         centroid: closestLoc,
     };
-}
-
-const SEATING_CHART_DROPPABLE_ID = "seating-chart";
-
-export function Canvas() {
-    const dropRef = useRef<HTMLDivElement | null>(null);
-
-    const addSeat = seatStore((s) => s.addSeat);
-    const addDelta = seatStore((s) => s.addDelta);
-    const offsets = seatStore((s) => s.offsets);
-    const setSeatOffset = seatStore((s) => s.setSeatOffset);
-
-    const setActive = useSetActive();
-    const setPreview = seatStore((s) => s.setPreview);
-
-    const stopDrag = seatStore((s) => s.stopDrag);
-    return (
-        <Dnd.Context
-            onDragEnd={handleDragEnd}
-            onDragStart={handleDragStart}
-            onDragMove={handleDragMove}
-        >
-            <Dnd.Droppable
-                id={SEATING_CHART_DROPPABLE_ID}
-                className="relative min-h-[600px] min-w-[80%] bg-white"
-            >
-                <div ref={dropRef}>
-                    <DropPreview />
-                    <Seats />
-                </div>
-                <Centroids />
-            </Dnd.Droppable>
-            <div className="h-[600px] min-w-28 border-l-2 border-l-black bg-white pl-4">
-                <DraggableSeat />
-            </div>
-        </Dnd.Context>
-    );
-
-    function handleDragStart(e: Dnd.DragStartEvent) {
-        const offset = offsets.get(e.active.id as id);
-        if (!offset) {
-            return;
-        }
-        console.log("drag start", offset);
-        const active = Object.assign(offset, { id: e.active.id as newId });
-        setActive(active);
-    }
-
-    function handleDragMove(e: Dnd.DragMoveEvent) {
-        // console.log("drag over", e)
-        const id = e.active.id as newId;
-
-        if (e.over === null && id === "new") {
-            setActive(null);
-            return;
-        }
-        const coords = getNonSnapPreviewCoords(dropRef, e);
-
-        setActive(Object.assign(coords, { id }));
-        setPreview(getDropPreviewCoords(dropRef, e));
-    }
-
-    function handleDragEnd(e: Dnd.DragEndEvent) {
-        console.log("drag end", e);
-        const id = parseId(e.active.id);
-        const snapCoords = getSnapCoords(dropRef, e);
-        if (id == "new") {
-            const coords = snapCoords ?? getNonSnapCoords(dropRef, e);
-            addSeat(coords.x, coords.y);
-            stopDrag();
-            return;
-        }
-        if (snapCoords) {
-            setSeatOffset(id, snapCoords);
-            return;
-        }
-        addDelta(id, e.delta);
-        stopDrag();
-    }
-}
-
-function parseId(id: number | string): newId {
-    if (typeof id === "number" || id === "new") {
-        return id;
-    }
-    const idInt = Number.parseInt(id);
-    if (!Number.isSafeInteger(idInt)) {
-        console.error(`invalid id ${id}`);
-        return "new";
-    }
-    return idInt;
-}
-
-function getDropPreviewCoords(
-    dzRef: RefObject<HTMLDivElement>,
-    dragEvent: Dnd.DragEvent,
-) {
-    const snapCoords = getSnapCoords(dzRef, dragEvent);
-    if (snapCoords === null) {
-        return getNonSnapPreviewCoords(dzRef, dragEvent);
-    }
-    return snapCoords;
-}
-
-const SNAP_THRESHOLD = 150;
-
-function getSnapCoords(
-    dzRef: RefObject<HTMLDivElement>,
-    dragEvent: Dnd.DragEvent,
-) {
-    const active = getActiveCentroid();
-    if (active === null) {
-        console.log("no active");
-        return null;
-    }
-    const activeDims = getActiveDims();
-    if (activeDims === null) {
-        console.log("no active dims");
-        return null;
-    }
-    const closest = getClosestCentroid(parseId(dragEvent.active.id));
-    if (closest === null) {
-        console.log("no closest");
-        return null;
-    }
-    if (closest.distance > SNAP_THRESHOLD) {
-        // console.log("closest too far", closest.distance);
-        return null;
-    }
-    const closestLoc = seatStore.getState().offsets.get(closest.id) ?? null;
-    if (!closestLoc) {
-        console.error("no closest loc");
-        return null;
-    }
-
-    let side: Side;
-    {
-        const a = active;
-        const b = closest.centroid;
-
-        const right = a.x > b.x;
-        const below = a.y > b.y;
-        const xCloser = Math.abs(a.x - b.x) < Math.abs(a.y - b.y);
-
-        if (xCloser) {
-            side = below ? "below" : "above";
-        } else {
-            side = right ? "right" : "left";
-        }
-    }
-
-    const dzDims = dzRef.current?.getBoundingClientRect();
-    if (!dzDims) {
-        console.log("no dz dims");
-        return null;
-    }
-
-    let coords: Point = {
-        x: activeDims.left,
-        y: activeDims.top,
-    };
-
-    switch (side) {
-        case "above":
-            coords.x = closestLoc.x;
-            coords.y = closestLoc.y - activeDims.height - SNAP_PAD;
-            break;
-        case "below":
-            coords.x = closestLoc.x;
-            coords.y = closestLoc.y + activeDims.height + SNAP_PAD;
-            break;
-        case "left":
-            coords.x = closestLoc.x - activeDims.width - SNAP_PAD;
-            coords.y = closestLoc.y;
-            break;
-        case "right":
-            coords.x = closestLoc.x + activeDims.width + SNAP_PAD;
-            coords.y = closestLoc.y;
-            break;
-    }
-
-    return coords;
-}
-
-type Side = "above" | "below" | "left" | "right";
-
-const SNAP_PAD = 5;
-
-function distance(a: Point, b: Point) {
-    return Math.floor(
-        Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)),
-    );
-}
-
-// NOTE: this is the offset controlled by browser or @dnd-kit (idk which) giving the
-// dragged element a lifted effect
-const DRAG_EXISTING_Y_OFFSET = 19;
-
-function getNonSnapPreviewCoords(
-    dzRef: RefObject<HTMLDivElement>,
-    dragEvent: Dnd.DragEvent,
-) {
-    const { x, y } = getNonSnapCoords(dzRef, dragEvent);
-    const yOffset = dragEvent.active.id === "new" ? 0 : DRAG_EXISTING_Y_OFFSET;
-    return { x, y: y - yOffset };
-}
-
-function getNonSnapCoords(
-    dzRef: RefObject<HTMLDivElement>,
-    dragEvent: Dnd.DragEvent,
-) {
-    const cbb = dzRef.current?.getBoundingClientRect();
-    const coords = calcCoordsDelta(dragEvent, cbb);
-    return coords;
-}
-
-function calcCoordsDelta(
-    e: Dnd.DragEvent,
-    dzOfs?: { left: number; top: number },
-) {
-    // FIXME: need to test whether all properties exist on touch event as well
-    const activator = e.activatorEvent as MouseEvent;
-    const origX = activator.clientX;
-    const origY = activator.clientY;
-    const delta = e.delta;
-    const dzOfsX = dzOfs?.left ?? 0;
-    const dzOfsY = dzOfs?.top ?? 0;
-    // NOTE: I believe accessing offsetX sets it in place
-    // (important when accessed in handleDragMove)
-    const mouseOfsX = activator.offsetX;
-    const mouseOfsY = activator.offsetY;
-    const x = origX + delta.x - dzOfsX - mouseOfsX;
-    const y = origY + delta.y - dzOfsY - mouseOfsY;
-    return { x, y };
-}
-
-function DropPreview() {
-    const active = seatStore((s) => s.preview);
-    return (
-        active && (
-            <div
-                className="absolute h-24 w-24 bg-blue-200"
-                style={{ left: active.x, top: active.y }}
-            ></div>
-        )
-    );
-}
-
-function DraggableSeat(props: { seatId?: id }) {
-    const id: newId = props.seatId ?? "new";
-    const offset = useSeatOffset(id);
-    return (
-        <Dnd.Draggable id={id + ""} data={{ id }}>
-            <Seat id={id} offset={offset} />
-        </Dnd.Draggable>
-    );
-}
-
-function Seats() {
-    const seats = useSeats();
-    return seats.map((s) => <DraggableSeat key={s} seatId={s} />);
-}
-
-function Seat(props: { id: newId; offset?: { x: number; y: number } }) {
-    const style = useSeatPos(props.offset);
-    const setSeatRef = useSetSeatRef(props.id);
-    return (
-        <div
-            ref={setSeatRef}
-            id={props.id + ""}
-            className="align-center h-24 w-24 border-2 border-black bg-white text-center text-black"
-            style={style}
-        >
-            {props.id}
-        </div>
-    );
-}
-
-function Centroids() {
-    const centroids = useCentroids();
-    return centroids.map(([id, c]) => (
-        <div
-            key={id}
-            className="absolute z-10 h-4 w-4 rounded-full bg-red-500"
-            style={{
-                left: c.x,
-                top: c.y,
-                transform: "translate(-50%, -50%)",
-            }}
-        ></div>
-    ));
-}
-
-function useSeatPos(seat?: { x: number; y: number }) {
-    const style = useMemo(() => {
-        if (!seat) {
-            return {
-                position: "unset" as const,
-            };
-        }
-        return {
-            position: "absolute" as const,
-            top: seat.y,
-            left: seat.x,
-        };
-    }, [seat]);
-
-    return style;
 }
