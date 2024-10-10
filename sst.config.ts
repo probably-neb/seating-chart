27b565605fc71f274de86cac60e5588f3f949118
Ben Kunkle <ben.kunkle@gmail.com>
2024-10-10 18:12:45 -0500

    create aws account

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,20 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "deskribe",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
-  },
-  async run() {},
+    app(input) {
+        return {
+            name: "deskribe",
+            removal: input?.stage === "production" ? "retain" : "remove",
+            home: "aws",
+            providers: {
+                aws: {
+                    profile:
+                        input.stage === "prod"
+                            ? "deskribe-prod"
+                            : "deskribe-dev",
+                },
+            },
+        };
+    },
+    async run() {},
 });
