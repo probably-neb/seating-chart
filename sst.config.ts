bb2f806f7356350e02284e7f29a6f64af2802c1d
Ben Kunkle <ben.kunkle@gmail.com>
2024-10-10 23:09:15 -0500

    add sst next app

--- sst.config.ts
+++ sst.config.ts
@@ -16,5 +16,22 @@ export default $config({
             },
         };
     },
-    async run() {},
+    async run() {
+        let domain: string;
+        if ($app.stage == "prod") {
+            domain = "deskribe.cc";
+        } else if ($app.stage == "dev") {
+            domain = "dev.deskribe.cc";
+        } else {
+            domain = $app.stage + ".dev.deskribe.cc"
+        }
+
+        const desk = new sst.aws.Nextjs("desk", {
+            path: "app/desk",
+            domain: "desk." + domain,
+            dev: {
+                command: "bun dev",
+            }
+        })
+    },
 });
